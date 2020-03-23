c340a25898c804f071d6e91931881d3931d34125
Anca <Anca@DESKTOP-I08MIGM>
2020-03-23 20:44:16 +0200

    Adaugare clasa la alegere

---
+++ GestiuneaStocurilorUneiFarmacii/src/Farmacie.java
@@ -0,0 +1,13 @@
+
+public class Farmacie {
+	private String nume;
+	private int nrProduse;
+	
+	public Farmacie(String nume, int nrProduse) {
+		super();
+		this.nume = nume;
+		this.nrProduse = nrProduse;
+	}
+	
+	
+}
