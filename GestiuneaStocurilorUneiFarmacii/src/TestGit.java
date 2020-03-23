dfdbe6f2347e1eca1a18907e239df3a5f1209064
Anca <Anca@DESKTOP-I08MIGM>
2020-03-23 21:09:01 +0200

    clas test

--- GestiuneaStocurilorUneiFarmacii/src/TestGit.java
+++ GestiuneaStocurilorUneiFarmacii/src/TestGit.java
@@ -1,4 +1,8 @@
 
 public class TestGit {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licență este [Gestiunea stocurilor unei farmacii]");
+		Farmacie farmacie= new Farmacie("Sanador", 100000000);
+		farmacie.descrieProduse();
+	}
 }
